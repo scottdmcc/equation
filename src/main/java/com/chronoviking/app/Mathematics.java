9c145db8eb362f2bb4a52f26b58d3db444193dea
smccollough <scott@chronoviking.com>
2019-05-23 18:34:02 -0700

    Lots of clean up. Added new features. Now tries to reformat the equation so a better chance of success. Made iteration through each equation a reusable method. Stores error messages in variable so no longer reliant on answer equaling -1. Added more unit tests.

--- src/main/java/com/chronoviking/app/Mathematics.java
+++ src/main/java/com/chronoviking/app/Mathematics.java
@@ -8,25 +8,35 @@ import org.apache.commons.lang3.ArrayUtils;
  */
 public class Mathematics {
     private String equation = null;
+    private int answer;
+    private String errorMsg = null;
 
     public static void main( String[] args ) {
         Mathematics problem = new Mathematics();
 
+        // If there are zero, or more than one argument, show the instructions and exit
         if (args.length != 1) {
             System.out.println("You must include a single argument of a math expression, enclosed in quotes.");
             System.out.println("Example: equation \"(1 + 2) * 3\"");
             System.exit(0);
         }
 
+        // Store the argument for usage
         problem.setEquation(args[0]);
 
-        if (problem.getEquation() == null) {
-            System.out.println("The given expression is not an equation this app can currently solve.");
-            System.out.println("Argument: " + args[0]);
+        // Check to see if the sanitation produced an error
+        if (problem.getErrorMsg() != null) {
+            System.out.println("The given expression is not an equation this app can currently reduce.");
+            System.out.println("Expression: " + args[0]);
+            System.out.println("Error: " + problem.getErrorMsg());
             System.exit(0);
         }
 
-        System.out.println(problem.getEquation() + " = " + problem.parse());
+        // Solve the problem
+        problem.solve();
+
+        // Print results to the console
+        System.out.println(problem.getEquation() + " = " + problem.getAnswer());
     }
 
     protected void setEquation(String equation) {
@@ -35,26 +45,42 @@ public class Mathematics {
     protected String getEquation() {
         return this.equation;
     }
-
     private String sanitize(String equation) {
-        // For right now, this just makes sure there are no unexpected characters
-        // Future updates would allow much more robust validation and sanitation
         String cleanEquation = equation;
 
+        // Make sure there are no unexpected characters
         if (!equation.matches("^[0-9()\\+\\* ]+$")) {
+            setErrorMsg("Foreign characters in the equation");
             return null;
         }
+
+        // Remove all spaces, and then only put them back on either side of operands
+        cleanEquation = cleanEquation.replace(" ", "");
+        cleanEquation = cleanEquation.replaceAll("([\\+\\*])", " $1 ");
+
+        // Future updates can also test for mismatched parenthesis and other errors
 
         return cleanEquation;
     }
 
-    protected int parse() {
-        String equation = getEquation();
+    private void setAnswer(int answer) {
+        this.answer = answer;
+    }
+    protected int getAnswer() {
+        return this.answer;
+    }
 
-        if (equation == null) {
-            return -1;
-        }
+    private void setErrorMsg(String msg) {
+        this.errorMsg = msg;
+    }
+    protected String getErrorMsg() {
+        return errorMsg;
+    }
 
+    private void solve() {
+        parse(getEquation());
+    }
+    protected void parse(String equation) {
         // Define parenthesis
         final char OPEN_CHAR = '(';
         final char CLOSE_CHAR = ')';
@@ -74,7 +100,7 @@ public class Mathematics {
             }
 
             // Solve inside the parenthesis
-            subtotal = solve(equation.substring(open_pos, close_pos));
+            subtotal = reduce(equation.substring(open_pos, close_pos));
 
             // Remove the parenthesis and the inner equation and substitute in the subtotal
             equation = equation.replace(OPEN_CHAR + equation.substring(open_pos, close_pos) + CLOSE_CHAR, Integer.toString(subtotal));
@@ -84,51 +110,58 @@ public class Mathematics {
             close_pos = equation.indexOf(CLOSE_CHAR);
         }
 
-        // By now we have removed all the parenthesis, so solve the final simplified equation
-        return solve(equation);
+        // By now we have removed all the parenthesis, so reduce the final simplified equation
+        setAnswer(reduce(equation));
     }
-    private int solve(String equation) {
+    private int reduce(String equation) {
         // Set up variables
         int total = 0;
         String[] factors = equation.split(" ");
-        boolean multiply;
 
-        // Multiplication first; can do the same for division
+        // Go through the equation, searching for all multiplication operations
+        factors = evaluateOperation("*", factors);
+
+        // Go through the equation, searching for all addition operations
+        factors = evaluateOperation("+", factors);
+
+        // Return the final value
+        return Integer.parseInt(factors[0]);
+    }
+    private String[] evaluateOperation(String operandChar, String[] factors) {
+        boolean operandExists;
+        int primary;
+        int secondary;
+
         do {
-            // Assume there are no multiplication parts left
-            multiply = false;
+            // Assume there are no operations to perform
+            operandExists = false;
             for (int i = 0; i < factors.length - 1; i++) {
-                // Look through the array of elements for the multiplication symbol
-                if (factors[i].equals("*")) {
-                    // If found solve and replace the element before the symbol
-                    // Converting the string to ints and back again
-                    factors[i - 1] = Integer.toString(Integer.parseInt(factors[i - 1]) * Integer.parseInt(factors[i + 1]));
+                // Look through the array of elements for the operand symbol
+                if (factors[i].equals(operandChar)) {
+                    // If found, get the int value of the index before and after the symbol
+                    primary = Integer.parseInt(factors[i - 1]);
+                    secondary = Integer.parseInt(factors[i + 1]);
+                    // Based on the symbol, do the math and convert back to a string in the index pos before the symbol
+                    switch (operandChar) {
+                        case "*":
+                            factors[i - 1] = Integer.toString(primary * secondary);
+                            break;
+                        case "+":
+                            factors[i - 1] = Integer.toString(primary + secondary);
+                            break;
+                    }
+
                     // Remove the symbol
                     factors = ArrayUtils.remove(factors, i);
                     // Remove the element past the symbol; but because we deleted one, don't remove i + 1 as it's now the new i
                     factors = ArrayUtils.remove(factors, i);
-                    // Signify we found at least one more multiplication to evaluate
-                    multiply = true;
+                    // Signify we found at least one more operand to evaluate
+                    operandExists = true;
                     // Start over; can't continue as we forcibly changed the length of the array
                     break;
                 }
             }
-        } while (multiply);
-
-        // Works the same as multiplication, except we can assume there is only addition left so we don't need the boolean
-        do {
-            for (int i = 0; i < factors.length - 1; i++) {
-                if (factors[i].equals("+")) {
-                    factors[i - 1] = Integer.toString(Integer.parseInt(factors[i - 1]) + Integer.parseInt(factors[i + 1]));
-                    factors = ArrayUtils.remove(factors, i);
-                    factors = ArrayUtils.remove(factors, i);
-                    break;
-                }
-            }
-            // Instead, test that there is more than a single element to the array left
-        } while (factors.length > 1);
-
-        // Return the final value
-        return Integer.parseInt(factors[0]);
+        } while (operandExists);
+        return factors;
     }
 }
--- src/test/java/com/chronoviking/app/MathematicsTest.java
+++ src/test/java/com/chronoviking/app/MathematicsTest.java
@@ -1,40 +1,89 @@
 package com.chronoviking.app;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Assert;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for math app.
  */
 public class MathematicsTest
 {
     /**
-     * Rigorous Test :-)
+     * Simple tests
      */
     @Test
-    public void shouldAnswerWithTrue() {
+    public void testSanitation() {
         Mathematics problem = new Mathematics();
+        String equation;
+
+        equation = "1 + 1";
+        problem.setEquation(equation);
+        Assert.assertNull(equation + " is valid", problem.getErrorMsg());
+
+        equation = "(2 + 3) + 5";
+        problem.setEquation(equation);
+        Assert.assertNull(equation + " is valid", problem.getErrorMsg());
+
+        equation = "1 + 2 * 3";
+        problem.setEquation(equation);
+        Assert.assertNull(equation + " is valid", problem.getErrorMsg());
+
+        equation = "(3 + 4) * 6";
+        problem.setEquation(equation);
+        Assert.assertNull(equation + " is valid", problem.getErrorMsg());
+
+        equation = "apple + (5 * 2)";
+        problem.setEquation(equation);
+        Assert.assertNotNull(equation + " is not valid", problem.getErrorMsg());
+    }
+
+    @Test
+    public void testFixingFormatting() {
+        Mathematics problem = new Mathematics();
+        String before;
+        String after;
+
+        before = "1+1"; after = "1 + 1";
+        problem.setEquation(before);
+        Assert.assertEquals("'" + before + "' becomes '" + after + "'", after, problem.getEquation());
+
+        before = " 1  +  1 "; after = "1 + 1";
+        problem.setEquation(before);
+        Assert.assertEquals("'" + before + "' becomes '" + after + "'", after, problem.getEquation());
+
+        before = "(2+3) + 5"; after = "(2 + 3) + 5";
+        problem.setEquation(before);
+        Assert.assertEquals("'" + before + "' becomes '" + after + "'", after, problem.getEquation());
+
+        before = " ( 1 * 4 ) + ( 5 * 2 ) "; after = "(1 * 4) + (5 * 2)";
+        problem.setEquation(before);
+        Assert.assertEquals("'" + before + "' becomes '" + after + "'", after, problem.getEquation());
+    }
+
+    @Test
+    public void testSolvingEquations() {
+        Mathematics problem = new Mathematics();
+        String equation;
         int answer;
-        System.out.println("Running tests...");
 
-        problem.setEquation("1 + 1"); answer = 2;
-        Assert.assertEquals(problem.getEquation() + " = " + answer, answer, problem.parse());
+        equation = "1 + 1"; answer = 2;
+        problem.parse(equation);
+        Assert.assertEquals(equation + " = " + answer, answer, problem.getAnswer());
 
-        problem.setEquation("(2 + 3) + 5"); answer = 10;
-        Assert.assertEquals(problem.getEquation() + " = " + answer, answer, problem.parse());
+        equation = "(2 + 3) + 5"; answer = 10;
+        problem.parse(equation);
+        Assert.assertEquals(equation + " = " + answer, answer, problem.getAnswer());
 
-        problem.setEquation("1 + 2 * 3"); answer = 7;
-        Assert.assertEquals(problem.getEquation() + " = " + answer, answer, problem.parse());
+        equation = "1 + 2 * 3"; answer = 7;
+        problem.parse(equation);
+        Assert.assertEquals(equation + " = " + answer, answer, problem.getAnswer());
 
-        problem.setEquation("(3 + 4) * 6"); answer = 42;
-        Assert.assertEquals(problem.getEquation() + " = " + answer, answer, problem.parse());
+        equation = "(3 + 4) * 6"; answer = 42;
+        problem.parse(equation);
+        Assert.assertEquals(equation + " = " + answer, answer, problem.getAnswer());
 
-        problem.setEquation("(1 * 4) + (5 * 2)"); answer = 14;
-        Assert.assertEquals(problem.getEquation() + " = " + answer, answer, problem.parse());
-
-        problem.setEquation("apple + (5 * 2)"); answer = -1;
-        Assert.assertEquals(problem.getEquation() + " is not a valid equation", answer, problem.parse());
+        equation = "(1 * 4) + (5 * 2)"; answer = 14;
+        problem.parse(equation);
+        Assert.assertEquals(equation + " = " + answer, answer, problem.getAnswer());
     }
 }
